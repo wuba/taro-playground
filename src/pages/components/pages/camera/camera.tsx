9f02275ff41b1a1e846d051ef3af3f10292b7c4e
eli <523753042@qq.com>
2021-09-22 10:17:53 +0800

    feat: 补充了pick和camera的额外功能demo (#4)
    * feat: 相机添加扫码模式demo
    
    * feat: picker添加object[]的demo
    
    * feat: remove useless code
    
    * refactor: 重写相机扫码部分代码
    
    Co-authored-by: xiangtianyu <xiangtianyu@58.com>

--- src/pages/components/pages/camera/camera.tsx
+++ src/pages/components/pages/camera/camera.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import Taro from "@tarojs/taro";
-import { Camera, Button, View, Image, Video } from "@tarojs/components";
-import { hadlePermissionsDeny } from '@/utils/index'
+import { Camera, Button, View, Image, Video, Switch } from "@tarojs/components";
+import { throttle, hadlePermissionsDeny } from '@/utils/index'
 import Header from "@/components/head/head";
+import { CameraProps } from "@tarojs/components/types/Camera";
 
 import "./camera.scss";
 
-interface IProps {}
+interface IProps { }
 
 interface IState {
   devicePosition: 'front' | 'back',
   imageSrc: string,
   videoUrl: string,
+  mode: keyof CameraProps.mode,
 }
 
 export default class PageView extends React.Component<IProps, IState> {
   constructor(props) {
     super(props);
     this.state = {
+      mode: 'normal',
       devicePosition: "back",
       imageSrc: "",
       videoUrl: ""
@@ -104,28 +107,41 @@ export default class PageView extends React.Component<IProps, IState> {
       }
     });
   };
+  changeMode = () => {
+    this.setState({
+      mode: this.state.mode === 'normal' ? 'scanCode' : 'normal'
+    })
+  }
+  onScanCode = throttle((e) => {
+    Taro.showToast({ title: `扫码成功${e.detail.result}`, icon: 'none', duration: 4000 })
+  }, 5000)
 
   render() {
-    const { imageSrc, devicePosition, videoUrl } = this.state;
+    const { imageSrc, devicePosition, videoUrl, mode } = this.state;
     return (
       <View className="components-page">
         <View className="components-page__header">
-          <Header title="Camera" />
+          <Header title={`Camera当前是${mode === 'normal' ? '正常模式' : '扫码模式'}`} />
         </View>
         <View className="components-page__body">
           <View className="components-page__body-example example">
             <View className="example-body">
               <Camera
+                mode={mode}
                 id="camera"
                 className="cammer-content"
-                ref={this.ref}
+                ref={(ref) => this.ref = ref}
                 onStop={this.handleStop}
                 onError={this.handleError}
                 devicePosition={this.state.devicePosition}
                 onInitDone={() => {
                   this.cameraContext = Taro.createCameraContext();
                 }}
-              />
+                onScanCode={this.onScanCode} />
+            </View>
+            <View className="switch-list__item">
+              <View className="switch-list__text">扫码模式</View>
+              <Switch checked={mode === 'scanCode'} onChange={this.changeMode}></Switch>
             </View>
             <Button className="btn" type="primary" onClick={this.toggleDevice}>
               开启{devicePosition == "back" ? "前置" : "后置"}镜头
@@ -139,7 +155,6 @@ export default class PageView extends React.Component<IProps, IState> {
             <Button className="btn" type="primary" onClick={this.stopRecord}>
               停止录像
             </Button>
-
             {imageSrc && <Image className="preview" src={imageSrc} />}
             {videoUrl && <Video className="preview" src={videoUrl} autoplay />}
           </View>
