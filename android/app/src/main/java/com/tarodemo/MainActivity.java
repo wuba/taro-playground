9b3f80c9f10e0aa89732844ab19bcdece97444ce
zhiqingchen <329604487@qq.com>
2022-05-27 20:06:17 +0800

    feat: sync from 0.68 template -3

--- android/app/src/main/java/com/tarodemo/MainActivity.java
+++ android/app/src/main/java/com/tarodemo/MainActivity.java
@@ -1,11 +1,12 @@
 package com.tarodemo;
 import expo.modules.ReactActivityDelegateWrapper;
-import com.facebook.react.ReactActivityDelegate;
-import android.os.Bundle; // here
-import com.facebook.react.ReactActivity;
-import com.tarodemo.devmanager.TaroDevManager;
 
-import org.devio.rn.splashscreen.SplashScreen; // here
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,13 +14,6 @@ public class MainActivity extends ReactActivity {
   protected void onCreate(Bundle savedInstanceState) {
     SplashScreen.show(this);
     super.onCreate(null);
-  }
-
-  @Override
-  public void finish() {
-    // clear ReactNativeHost on Activity finish
-    TaroDevManager.clearReactNativeHost();
-    super.finish();
   }
 
   /**
@@ -31,10 +25,26 @@ public class MainActivity extends ReactActivity {
     return "taroDemo";
   }
 
+  /**
+   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
+   * you can specify the rendered you wish to use (Fabric or the older renderer).
+   */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegateWrapper(this,
-      new ReactActivityDelegate(this, getMainComponentName())
-    );
+    return new ReactActivityDelegateWrapper(this, new MainActivityDelegate(this, getMainComponentName()));
+  }
+
+  public static class MainActivityDelegate extends ReactActivityDelegate {
+    public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
+      super(activity, mainComponentName);
+    }
+
+    @Override
+    protected ReactRootView createRootView() {
+      ReactRootView reactRootView = new ReactRootView(getContext());
+      // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+      reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
+      return reactRootView;
+    }
   }
 }
