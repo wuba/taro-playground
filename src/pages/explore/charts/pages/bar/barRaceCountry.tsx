923f8c7a2d7e2c965dd19707e929992344341a62
zhiqingchen <329604487@qq.com>
2022-12-15 19:19:13 +0800

    fix: bar race country

--- src/pages/explore/charts/pages/bar/barRaceCountry.tsx
+++ src/pages/explore/charts/pages/bar/barRaceCountry.tsx
@@ -1,5 +1,4 @@
 import { setNavigationBarTitle, request } from '@tarojs/taro';
-// import * as echarts from 'echarts/core';
 import { useCallback, useEffect } from 'react';
 import { ROOT_PATH } from '../../constant';
 import Chart from '../../echarts';
@@ -59,125 +58,125 @@ export default function Index() {
       requestData(reqEmojiFlagsUrl),
       requestData(reqLifeExpectancyUrl)
     ]).then(([res0, res1]) => {
-      // const flags = res0?.data[0];
-      // const data = res1?.data[0];
-      // const years: any = [];
-      // for (let i = 0; i < data.length; ++i) {
-      //   if (years.length === 0 || years[years.length - 1] !== data[i][4]) {
-      //     years.push(data[i][4]);
-      //   }
-      // }
-      // function getFlag(countryName) {
-      //   if (!countryName) {
-      //     return '';
-      //   }
-      //   return (
-      //     flags.find(function(item) {
-      //       return item.name === countryName;
-      //     }) || {}
-      //   ).emoji;
-      // }
-      // let startIndex = 10;
-      // let startYear = years[startIndex];
-      // const option = {
-      //   grid: {
-      //     top: 10,
-      //     bottom: 30,
-      //     left: 150,
-      //     right: 80
-      //   },
-      //   xAxis: {
-      //     max: 'dataMax',
-      //     axisLabel: {
-      //       formatter: function(n) {
-      //         return Math.round(n) + '';
-      //       }
-      //     }
-      //   },
-      //   dataset: {
-      //     source: data.slice(1).filter(function(d) {
-      //       return d[4] === startYear;
-      //     })
-      //   },
-      //   yAxis: {
-      //     type: 'category',
-      //     inverse: true,
-      //     max: 10,
-      //     axisLabel: {
-      //       show: true,
-      //       fontSize: 14,
-      //       formatter: function(value) {
-      //         return value + '{flag|' + getFlag(value) + '}';
-      //       },
-      //       rich: {
-      //         flag: {
-      //           fontSize: 25,
-      //           padding: 5
-      //         }
-      //       }
-      //     },
-      //     animationDuration: 300,
-      //     animationDurationUpdate: 300
-      //   },
-      //   series: [
-      //     {
-      //       realtimeSort: true,
-      //       seriesLayoutBy: 'column',
-      //       type: 'bar',
-      //       itemStyle: {
-      //         color: function(param) {
-      //           return countryColors[param.value[3]] || '#5470c6';
-      //         }
-      //       },
-      //       encode: {
-      //         x: dimension,
-      //         y: 3
-      //       },
-      //       label: {
-      //         show: true,
-      //         precision: 1,
-      //         position: 'right',
-      //         valueAnimation: true,
-      //         fontFamily: 'monospace'
-      //       }
-      //     }
-      //   ],
-      //   // Disable init animation.
-      //   animationDuration: 0,
-      //   animationDurationUpdate: updateFrequency,
-      //   animationEasing: 'linear',
-      //   animationEasingUpdate: 'linear',
-      //   graphic: {
-      //     elements: [
-      //       {
-      //         type: 'text',
-      //         right: 160,
-      //         bottom: 60,
-      //         style: {
-      //           text: startYear,
-      //           font: 'bolder 80px monospace',
-      //           fill: 'rgba(100, 100, 100, 0.25)'
-      //         },
-      //         z: 100
-      //       }
-      //     ]
-      //   }
-      // };
-      // for (let i = startIndex; i < years.length - 1; ++i) {
-      //   (function(j) {
-      //     setTimeout(function() {
-      //       updateYear(years[j + 1]);
-      //     }, (i - startIndex) * updateFrequency);
-      //   })(i);
-      // }
-      // function updateYear(year) {
-      //   let source = data.slice(1).filter(function(d) {
-      //     return d[4] === year;
-      //   });
-      //   option.series[0].data = source;
-      //   option.graphic.elements[0].style.text = year;
-      //   myChart.setOption(option);
-      // }
+      const flags = res0?.data;
+      const data = res1?.data;
+      const years: any = [];
+      for (let i = 0; i < data.length; ++i) {
+        if (years.length === 0 || years[years.length - 1] !== data[i][4]) {
+          years.push(data[i][4]);
+        }
+      }
+      function getFlag(countryName) {
+        if (!countryName) {
+          return '';
+        }
+        return (
+          flags.find(function(item) {
+            return item.name === countryName;
+          }) || {}
+        ).emoji;
+      }
+      let startIndex = 10;
+      let startYear = years[startIndex];
+      const option = {
+        grid: {
+          top: 10,
+          bottom: 30,
+          left: 150,
+          right: 80
+        },
+        xAxis: {
+          max: 'dataMax',
+          axisLabel: {
+            formatter: function(n) {
+              return Math.round(n) + '';
+            }
+          }
+        },
+        dataset: {
+          source: data.slice(1).filter(function(d) {
+            return d[4] === startYear;
+          })
+        },
+        yAxis: {
+          type: 'category',
+          inverse: true,
+          max: 10,
+          axisLabel: {
+            show: true,
+            fontSize: 14,
+            formatter: function(value) {
+              return value + '{flag|' + getFlag(value) + '}';
+            },
+            rich: {
+              flag: {
+                fontSize: 25,
+                padding: 5
+              }
+            }
+          },
+          animationDuration: 300,
+          animationDurationUpdate: 300
+        },
+        series: [
+          {
+            realtimeSort: true,
+            seriesLayoutBy: 'column',
+            type: 'bar',
+            itemStyle: {
+              color: function(param) {
+                return countryColors[param.value[3]] || '#5470c6';
+              }
+            },
+            encode: {
+              x: dimension,
+              y: 3
+            },
+            label: {
+              show: true,
+              precision: 1,
+              position: 'right',
+              valueAnimation: true,
+              fontFamily: 'monospace'
+            }
+          }
+        ],
+        // Disable init animation.
+        animationDuration: 0,
+        animationDurationUpdate: updateFrequency,
+        animationEasing: 'linear',
+        animationEasingUpdate: 'linear',
+        graphic: {
+          elements: [
+            {
+              type: 'text',
+              right: 160,
+              bottom: 60,
+              style: {
+                text: startYear,
+                font: 'bolder 80px monospace',
+                fill: 'rgba(100, 100, 100, 0.25)'
+              },
+              z: 100
+            }
+          ]
+        }
+      };
+      for (let i = startIndex; i < years.length - 1; ++i) {
+        (function(j) {
+          setTimeout(function() {
+            updateYear(years[j + 1]);
+          }, (i - startIndex) * updateFrequency);
+        })(i);
+      }
+      function updateYear(year) {
+        let source = data.slice(1).filter(function(d) {
+          return d[4] === year;
+        });
+        option.series[0].data = source;
+        option.graphic.elements[0].style.text = year;
+        myChart.setOption(option);
+      }
     });
   }, []);
 
