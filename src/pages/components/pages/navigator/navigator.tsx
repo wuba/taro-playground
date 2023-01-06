7dc6513d227a1d05a96384e530e88d7fdf8420da
zhiqingchen <329604487@qq.com>
2023-01-06 17:38:05 +0800

    feat: add Navigator

--- src/pages/components/pages/navigator/navigator.tsx
+++ src/pages/components/pages/navigator/navigator.tsx
@@ -1,40 +1,57 @@
-import Taro from '@tarojs/taro'
-
-import { View, Navigator, Text } from '@tarojs/components'
+import { View, Navigator } from '@tarojs/components'
 
 import Header from '@/components/head/head'
 
 import './navigator.scss'
 
-export default () => {
+const url = '/pages/global/pages/router/index'
+const taburl = '/pages/global/index'
+
+const NavigatorPage = () => {
   return (
     <View className='components-page'>
       <View className='components-page__header'>
         <Header title='Navigator'></Header>
       </View>
-      <View className='components-page__bodyå'>
+      <View className='components-page__body'>
         <View className='components-page__body-example example'>
           <View className='example-body'>
-            {
-              Taro.getEnv() != Taro.ENV_TYPE.WEB ? <View className='example-body__navigators'>
-                <Navigator
-                  url='/pages/components/pages/navigatePage/navigatePage'
-                  className='example-body__navigators-item'
-                >
-                  <View className='example-body__navigator'>跳转到新页面</View>
-                </Navigator>
-                <Navigator
-                  url='/pages/component/pages/redirectPage/redirectPage'
-                  className='example-body__navigators-item'
-                  open-type='redirect'
-                >
-                  <View >在当前页打开</View>
-                </Navigator>
-              </View> : <Text>暂未支持，请使用Taro API</Text>
-            }
+            <View className='example-body__navigators'>
+              <Navigator
+                url={url}
+              >
+                <View className='example-body__navigators-item'>跳转到新页面</View>
+              </Navigator>
+              <Navigator
+                url={url}
+                openType='redirect'
+              >
+                <View className='example-body__navigators-item'>在当前页打开</View>
+              </Navigator>
+              <Navigator
+                url={taburl}
+                openType='switchTab'
+              >
+                <View className='example-body__navigators-item'>跳转到tabBar页面</View>
+              </Navigator>
+              <Navigator
+                url={url}
+                openType='reLaunch'
+              >
+                <View className='example-body__navigators-item'>关闭所有页面</View>
+              </Navigator>
+              <Navigator
+                delta={1}
+                openType='navigateBack'
+              >
+                <View className='example-body__navigators-item'>返回上一页面</View>
+              </Navigator>
+            </View>
           </View>
         </View>
       </View>
     </View>
   )
 }
+
+export default NavigatorPage
