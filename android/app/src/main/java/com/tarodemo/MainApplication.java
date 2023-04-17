9762c14caebcc9b16ba97fca087c238ca6d67228
zhiqingchen <329604487@qq.com>
2023-04-17 15:57:02 +0800

    feat: rn 0.71

--- android/app/src/main/java/com/tarodemo/MainApplication.java
+++ android/app/src/main/java/com/tarodemo/MainApplication.java
@@ -1,23 +1,16 @@
 package com.tarodemo;
-
-import android.app.Application;
-import android.content.Context;
 import android.content.res.Configuration;
-
-import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.ReactNativeHost;
-import com.facebook.react.config.ReactFeatureFlags;
-import com.facebook.soloader.SoLoader;
-import com.tarodemo.BuildConfig;
-import com.tarodemo.devmanager.TaroDevManager;
-import com.tarodemo.devmanager.TaroReactNativeHost;
-import com.tarodemo.newarchitecture.MainApplicationReactNativeHost;
-
-import java.lang.reflect.InvocationTargetException;
-
 import expo.modules.ApplicationLifecycleDispatcher;
 import expo.modules.ReactNativeHostWrapper;
+
+import android.app.Application;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.soloader.SoLoader;
+
+import com.tarodemo.devmanager.TaroDevManager;
+import com.tarodemo.devmanager.TaroReactNativeHost;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -25,31 +18,16 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final TaroReactNativeHost mTaroReactNativeHost =
     new TaroReactNativeHost(this);
-
   private final ReactNativeHost mReactNativeHost =
-    new ReactNativeHostWrapper(this, mTaroReactNativeHost);
-
-  private final TaroReactNativeHost mNewArchitectureTaroNativeHost =
-    new MainApplicationReactNativeHost(this);
-
-  private final ReactNativeHost mNewArchitectureNativeHost =
-    new ReactNativeHostWrapper(this, mNewArchitectureTaroNativeHost);
+      new ReactNativeHostWrapper(this, mTaroReactNativeHost);
 
   public TaroReactNativeHost getTaroReactNativeHost() {
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      return mNewArchitectureTaroNativeHost;
-    } else {
-      return mTaroReactNativeHost;
-    }
+    return mTaroReactNativeHost;
   }
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      return mNewArchitectureNativeHost;
-    } else {
-      return mReactNativeHost;
-    }
+    return mReactNativeHost;
   }
 
   @Override
@@ -57,46 +35,17 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     instance = this;
     TaroDevManager.init();
-    // If you opted-in for the New Architecture, we enable the TurboModule system
-    ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      // If you opted-in for the New Architecture, we load the native entry point for this app.
+      DefaultNewArchitectureEntryPoint.load();
+    }
+    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     ApplicationLifecycleDispatcher.onApplicationCreate(this);
   }
 
   public static MainApplication getInstance() {
     return instance;
-  }
-
-  /**
-   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-   *
-   * @param context
-   * @param reactInstanceManager
-   */
-  private static void initializeFlipper(
-      Context context, ReactInstanceManager reactInstanceManager) {
-    if (BuildConfig.DEBUG) {
-      try {
-        /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
-        Class<?> aClass = Class.forName("com.tarodemo.ReactNativeFlipper");
-        aClass
-            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-            .invoke(null, context, reactInstanceManager);
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (NoSuchMethodException e) {
-        e.printStackTrace();
-      } catch (IllegalAccessException e) {
-        e.printStackTrace();
-      } catch (InvocationTargetException e) {
-        e.printStackTrace();
-      }
-    }
   }
 
   @Override
