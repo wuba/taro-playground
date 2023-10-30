b01802b95edd64f1e71289144a9f842418d2f684
tony chen <tony.zq.chen@gmail.com>
2023-10-30 11:23:10 +0800

    fix: cert import

--- .github/scripts/import-certificate.sh
+++ .github/scripts/import-certificate.sh
@@ -7,9 +7,19 @@ security list-keychains -s build.keychain
 security default-keychain -s build.keychain
 security unlock-keychain -p "" build.keychain
 security set-keychain-settings
-security import <(echo $SIGNING_CERTIFICATE_P12_DATA | base64 --decode) \
+echo "Importing certificate..."
+if [ -z "$SIGNING_CERTIFICATE_P12_DATA" ]; then
+  echo "Error: SIGNING_CERTIFICATE_P12_DATA is not set"
+  exit 1
+fi
+
+if [ -z "$SIGNING_CERTIFICATE_PASSWORD" ]; then
+  echo "Error: SIGNING_CERTIFICATE_PASSWORD is not set"
+  exit 1
+fi
+security import <(echo "$SIGNING_CERTIFICATE_P12_DATA" | base64 --decode) \
                 -f pkcs12 \
                 -k build.keychain \
-                -P $SIGNING_CERTIFICATE_PASSWORD \
+                -P "$SIGNING_CERTIFICATE_PASSWORD" \
                 -T /usr/bin/codesign
-security set-key-partition-list -S apple-tool:,apple: -s -k "" build.keychain
+echo "Certificate imported successfully"
