06efe1434cb639f4ca36ab5b13565ebf77fceb65
ZakaryCode <zakarycode@gmail.com>
2023-12-14 19:42:34 +0800

    feat(harmony): 支持 harmony 版本编译

--- src/app.config.ts
+++ src/app.config.ts
@@ -83,7 +83,6 @@ const pages = [
   // 'pages/components/pages/canvas/canvas',
   // 'pages/components/pages/map/map',
   'pages/components/pages/navigator/navigator',
-  'pages/components/pages/virtual-list/virtual-list',
   'pages/components/pages/movable-view/movable-view',
   // 组件相关 end
   // api 相关 start
@@ -122,16 +121,25 @@ const pages = [
   'pages/explore/index'
 ];
 
+if (process.env.TARO_ENV !== 'harmony') {
+  pages.push(
+    'pages/components/pages/virtual-list/virtual-list',
+  );
+}
+
 const subpackages = [
-  {
-    root: 'pages/explore/animate',
-    pages: ['lottie/index']
-  },
   {
     root: 'pages/explore/others',
     pages: ['linear-gradient/index']
   }
 ];
+
+if (process.env.TARO_ENV !== 'harmony') {
+  subpackages.push({
+    root: 'pages/explore/animate',
+    pages: ['lottie/index']
+  })
+}
 
 if (process.env.TARO_ENV === 'rn') {
   subpackages.push({
@@ -379,6 +387,7 @@ export default {
   pages: process.env.TARO_ENV === 'rn' ? pages : pages.splice(1),
   subpackages,
   window: {
+    backgroundColor: '#efefef',
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
---
+++ src/platform/index.harmony.ts
@@ -0,0 +1,3 @@
+export const hairlineWidth = '0.5px'
+export const NativeModules = undefined
+export const Linking = undefined
