82772dbfbfb509b64a0fbc16a7a36e86f569edb9
xuanzebin <xuanzebin3@jd.com>
2023-12-21 15:56:28 +0800

    feat: 更新 demo

--- src/app.config.ts
+++ src/app.config.ts
@@ -404,11 +404,11 @@ export default {
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
   },
-  // tabBar: {
-  //   color: '#333333',
-  //   selectedColor: '#6190E8', // 主题色
-  //   list: process.env.TARO_ENV === 'rn' ? tabs : tabs.splice(1)
-  // },
+  tabBar: {
+    color: '#333333',
+    selectedColor: '#6190E8', // 主题色
+    list: process.env.TARO_ENV === 'rn' ? tabs : tabs.splice(1)
+  },
   rn: {
     useNativeStack: true // 使用 @react-navigation/native-stack
   }
