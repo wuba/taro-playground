fafe0f0d1860f71878fbb0ddd2ab9b66bba57f68
tony chen <tony.zq.chen@gmail.com>
2024-01-24 18:34:35 +0800

    feat: add chooseMedia

--- src/pages/apis/pages/media/video/index.tsx
+++ src/pages/apis/pages/media/video/index.tsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from 'react';
 import Taro from '@tarojs/taro';
-import { View, Video, Button } from '@tarojs/components';
+import { View, Video, Button, Image } from '@tarojs/components';
 import { hadlePermissionsDeny } from '@/utils/index'
 
 import "./index.scss";
@@ -15,19 +15,45 @@ const Index = () => {
   const videoContext = useRef<any>({});
 
   const [filePath, setFilePage] = useState(path);
+  const [fileType, setFileType] = useState('video');
 
   const _onLoadedMetaData = () => {
     videoContext.current = Taro.createVideoContext("video");
   }
 
   const _chooseVideo = () => {
+    Taro.showLoading({})
     Taro.chooseVideo({
       sourceType: ['album', 'camera'],
       maxDuration: 60,
       camera: 'back',
     }).then((res: any) => {
+      Taro.hideLoading({})
       setFilePage(res.tempFilePath);
+      setFileType('video');
     }).catch(err => {
+      Taro.hideLoading({})
+      if (err.errMsg === 'Permissions denied!') {
+        hadlePermissionsDeny({
+          perssionText: '相机或照片'
+        })
+      }
+    })
+  }
+
+  const _chooseMedia = () => {
+    Taro.showLoading({})
+    Taro.chooseMedia({
+      count: 2,
+      mediaType: ['image','video'],
+      maxDuration: 60,
+      camera: 'back',
+    }).then((res: any) => {
+      Taro.hideLoading({})
+      setFilePage(res.tempFilePaths[0]);
+      setFileType(res.tempFiles[0].fileType);
+    }).catch(err => {
+      Taro.hideLoading({})
       if (err.errMsg === 'Permissions denied!') {
         hadlePermissionsDeny({
           perssionText: '相机或照片'
@@ -52,7 +78,7 @@ const Index = () => {
 
   return (
     <View className='api-page'>
-      <Video
+      {fileType === 'video' && <Video
         id="video"
         className='preview'
         onLoadedMetaData={_onLoadedMetaData}
@@ -66,13 +92,23 @@ const Index = () => {
         onLoad={() => {
           videoContext.current = Taro.createVideoContext("video");
         }}
-      ></Video>
+      ></Video>}
+      {fileType === 'image' && <Image
+        className='preview'
+        src={filePath}
+        mode='aspectFit'
+      ></Image>}
       <View className='api-page-body'>
         <Button
           type="primary"
           className="api-page-btn-success"
           onClick={_chooseVideo}
         >Taro.chooseVideo</Button>
+        <Button
+          type="primary"
+          className="api-page-btn-success"
+          onClick={_chooseMedia}
+        >Taro.chooseMedia</Button>
         <Button
           type="primary"
           className="api-page-btn-success"
